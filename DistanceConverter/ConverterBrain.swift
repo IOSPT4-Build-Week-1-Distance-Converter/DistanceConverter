ed01f6c80ad3de3e03a93dc77eec7ad098fc43eb
Christopher Price <christopher.r.price@gmail.com>
2019-12-18 23:50:48 -0500

    Added the enum and the stubs for the from() and to() methods.

--- DistanceConverter/ConverterBrain.swift
+++ DistanceConverter/ConverterBrain.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+enum Units: String {
+    case mile
+    case yard
+    case feet
+    case inch
+    case kilometer
+    case meter
+    case decimeter
+    case centimeter
+    case millimeter
+    case nauticalMile
+    case fathom
+    case league
+    case furlong
+    case rod
+    case chain
+}
+
+/*
+class ConverterBrain {
+    
+    func from() { // convert from user input and desired input units into meters.
+        switch <#value#> {
+        case mile:
+            
+        default:
+            <#code#>
+        }
+    
+    }
+    
+    func to() { // convert from meters into the user's desired output unit.
+        switch <#value#> {
+        case <#pattern#>:
+        <#code#>
+        default:
+        <#code#>
+        }
+    }
+}
+*/
