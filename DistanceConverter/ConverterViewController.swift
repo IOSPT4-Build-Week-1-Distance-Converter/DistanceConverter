3d5473ff9026f31fff2bb4b2f3da6de1ac82fb69
Waseem <idelbiw@gmail.com>
2019-12-19 22:10:14 -0500

    finished the main storyboard with constraints and created IBOutlets

--- DistanceConverter/ConverterViewController.swift
+++ DistanceConverter/ConverterViewController.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class ConverterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var numberToConvert: UITextField!
+    @IBOutlet var result: UILabel!
+    @IBOutlet var covertFromPickerView: UIPickerView!
+    @IBOutlet var convertToPickerView: UIPickerView!
+    
+    @IBAction func convertButtonTapped(_ sender: Any) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    
 }
